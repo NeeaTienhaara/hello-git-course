a03602b4e36b56e6061a627c27524d6f5f1b67ab
NeeaTienhaara <101215812+NeeaTienhaara@users.noreply.github.com>
2022-03-09 10:46:43 +0200

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
 #COMPC.CS.100 Ohjelmointi 1
 #Tekijä: Neea Tienhaara
 #Opiskelijanumero: 050009938
 
 def main():
     print("Hello World!")
-
-=======
-print("Hello world!")
->>>>>>> 9bb7a505374c63502be00fe0f9bbfff438b6a484
